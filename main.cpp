9f235561ad8dd241a799c900f72203d3837302ca
Евгений Панченко <worldbatlemailru@gmail.com>
2025-04-25 23:50:41 +0300

    Добавлена заготовка под две задачи

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void task1() {
+    std::cout << "\n--- Task 1 ---\n";
+}
+
+void task2() {
+    std::cout << "\n--- Task 2 ---\n";
+}
+
+int main() {
+    task1();
+    task2();
+    return 0;
+}
